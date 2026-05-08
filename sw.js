ca0a724ee51f15324eeadd95d33a3f4e2c1bd570
Kazue Kurosaki <137025030+kazuekurosaki@users.noreply.github.com>
2026-05-08 16:25:42 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'hikari-v3';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
